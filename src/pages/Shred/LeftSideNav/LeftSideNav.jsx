9257a44452bdc9101c8336285f66bcce162ecbeb
tahfeezmizan <fullname@gmail.com>
2024-04-04 04:31:36 +0600

    fetch categories data and display

--- src/pages/Shred/LeftSideNav/LeftSideNav.jsx
+++ src/pages/Shred/LeftSideNav/LeftSideNav.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 
 const LeftSideNav = () => {
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        fetch('categories.json')
+            .then(res => res.json())
+            .then(data => setCategories(data))
+    }, [])
+
     return (
         <div>
-            <h1 className="text-3xl">Left Side</h1>
+            <h1 className="text-3xl">All Categories</h1>
+            {
+                categories.map(categori => <Link
+                    className="flex px-14 py-4"
+                    key={categori.id}>
+                    {categori.name} </Link>)
+            }
         </div>
     );
 };
