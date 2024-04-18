37dd6c95ba9a8c2df293308295f5d444c0c1239d
tahfeezmizan <mizanahmed5316@gmail.com>
2024-04-18 11:56:27 +0600

    added new somethign

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import routes from './Routes/Routes.jsx'
+import './index.css'
 import AuthProvider from './provider/AuthProvider.jsx'
 
 
